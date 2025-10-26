0cb1c86d5a6909da3451453fed713564968c9e6e
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-10-26 17:29:18 -0500

    chore: update expo version and change API endpoint URLs
    - Updated expo dependency from version 53.0.22 to 54.0.12 in package.json.
    - Changed API endpoint in categoryService.tsx from "http://192.168.2.3:5000/api/v1" to "http://172.28.128.1:5000/api/v1".
    - Updated API endpoint in productService.ts from "http://192.168.2.3:5000/api/v1" to "http://172.28.128.1:5000/api/v1".

--- services/categoryService.tsx
+++ services/categoryService.tsx
@@ -2,7 +2,7 @@ import { Subcategory } from "@/interfaces";
 import { AxiosAdapter } from "../api/AxiosAdapter";
 import { HttpAdapter } from "../api/HttpAdapter";
 
-const http: HttpAdapter = new AxiosAdapter("http://192.168.2.3:5000/api/v1");
+const http: HttpAdapter = new AxiosAdapter("http://172.28.128.1:5000/api/v1");
 // const http: HttpAdapter = new AxiosAdapter("http://192.168.2.3:5000/api/v1");
 //
 // 🚨 La función ahora recibe 'categoryId' como parámetro
--- services/productService.ts
+++ services/productService.ts
@@ -4,7 +4,7 @@ import { Category } from "../domain/Category";
 import { IProduct } from "../domain/Product";
 import { Product } from '../interfaces/index';
 
-const http: HttpAdapter = new AxiosAdapter("http://192.168.2.3:5000/api/v1");
+const http: HttpAdapter = new AxiosAdapter("http://172.28.128.1:5000/api/v1");
 // const http: HttpAdapter = new AxiosAdapter("http://localhost:5000/api/v1");
 // 
 export const getProducts = async (): Promise<IProduct[]> => {
